f064f5efe573c1da0b6e31650735445f6340e819
abhishek-biz <abhishekdwivedi84869@gmail.com>
2026-09-19 13:33:20 +0530

    MAJOR OVERHAUL OF THE WEBSITE

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-this is for the reccord
+No AI is used or ever will be used in my writing. 
+![alt text](pulse.png)
---
+++ js/script.js
@@ -0,0 +1,4 @@
+/*
+  No JavaScript is required for The Pulse at present. This retained placeholder
+  documents that the former colour-mode interaction was intentionally removed.
+*/
